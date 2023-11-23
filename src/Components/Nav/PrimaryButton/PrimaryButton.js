8a2749ef3009881b166d57df5776e3ff84fd3034
Maciej-Clanet <maciejclanet@gmail.com>
2023-11-23 01:04:53 +0000

    fonts

--- src/Components/Nav/PrimaryButton/PrimaryButton.js
+++ src/Components/Nav/PrimaryButton/PrimaryButton.js
@@ -2,7 +2,7 @@ import "./PrimaryButton.css"
 
 const PrimaryButton = ({text, icon, action}) =>{
     
-    return <button className="primary-button" onClick={action}>{icon}<span>{text}</span></button>
+    return <button className="primary-button" onClick={action}>{icon}<span className="text-label prominent">{text}</span></button>
 }
 
 export default PrimaryButton;
