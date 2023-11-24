13731c3cf2567d0cf4ca3863710a8b4a7be3633e
Maciej-Clanet <maciejclanet@gmail.com>
2023-11-24 00:12:30 +0000

    Featured artist component added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,19 @@
 import "./Home.css"
 import SectionHeading from "../Components/SectionHeading/SectionHeading";
 import ThemeToggleButton from "../Components/ThemeToggleButton/ThemeToggleButton";
+import FeaturedArtistThumbnail from "../Components/FeaturedArtistThumbnail/FeaturedArtistThumbnail";
+import tempArtistImage from "../Assets/Temp/FeaturedArtist.png"
+
 const Home = () => {
     return (
         <>
+            <FeaturedArtistThumbnail artistName="goober" artistProfession="hoe" profileLink="/user/goober" image={tempArtistImage}/>
             <SectionHeading text="my heading"/>
             <SectionHeading text="This is a longer heading" active={true}/>
-           <ThemeToggleButton/>
 
-            Home page
+
+            
+
         </>
     )
 }
