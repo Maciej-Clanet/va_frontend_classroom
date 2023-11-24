6a6fb675604ae0eb8d77f1fdef0458c06e874872
Maciej-Clanet <maciejclanet@gmail.com>
2023-11-24 00:59:34 +0000

    thumbnails

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,14 +3,21 @@ import SectionHeading from "../Components/SectionHeading/SectionHeading";
 import ThemeToggleButton from "../Components/ThemeToggleButton/ThemeToggleButton";
 import FeaturedArtistThumbnail from "../Components/FeaturedArtistThumbnail/FeaturedArtistThumbnail";
 import tempArtistImage from "../Assets/Temp/FeaturedArtist.png"
+import ArtworkThumbnail from "../Components/ArtworkThumbnail/ArtworkThumbnail";
+import tempArtworkThumbnail from "../Assets/Temp/ArtworkThumb.png"
+
+import ProductCard from "../Components/ProductCard/ProductCard";
+import ProductImage from "../Assets/Temp/ProductThumbnail.png";
+import AvatarImage from "../Assets/Temp/Avatar.png"
 
 const Home = () => {
     return (
         <>
             <FeaturedArtistThumbnail artistName="goober" artistProfession="hoe" profileLink="/user/goober" image={tempArtistImage}/>
-            <SectionHeading text="my heading"/>
-            <SectionHeading text="This is a longer heading" active={true}/>
-
+            <SectionHeading text="Trending this week"/>
+            <ArtworkThumbnail image={tempArtworkThumbnail} artist="goober" title="test" imageLink="/test/image" />
+            <SectionHeading text="Popular on the Marketplace" active={true}/>
+            <ProductCard image={ProductImage} title="Test title" artist="goober" price={3.50} avatar={AvatarImage} productLink="/products/testproduct"/>
 
             
 
