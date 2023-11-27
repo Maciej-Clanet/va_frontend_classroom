0758fb82d3f29fb47ed45aa9a08d45b1c8690cb8
Maciej-Clanet <maciejclanet@gmail.com>
2023-11-27 23:51:28 +0000

    art grid stuff

--- src/Components/ArtGrid/ArtGrid.js
+++ src/Components/ArtGrid/ArtGrid.js
@@ -5,34 +5,28 @@ import tempArtworkThumbnail from "../../Assets/Temp/ArtworkThumb.png"
 import { useEffect } from "react"
 import { useState } from "react"
 
-const ArtGrid = ({rows}) => {
-    
+const ArtGrid = ({ rows }) => {
     const [thumbnails, setThumbnails] = useState([]);
 
-    function getMaxPerRow(){
+    function getMaxPerRow() {
         const cardWidth = 211;
         const gapSize = 8;
-        const padding = 32;
+        const padding = 16;
 
-        // const container = document.querySelector(".art-grid");
         const totalWidth = window.innerWidth;
-        let maxItems = Math.floor(totalWidth / (cardWidth + gapSize))
-        
-        while ((maxItems - 1) * gapSize + maxItems * cardWidth + padding > totalWidth) {
-            console.log("adjusting")
-            maxItems--;
-        }
+        let maxItems = Math.floor(totalWidth / (cardWidth + gapSize + padding))
 
         return maxItems
     }
 
-    function updateThumbnails(){    
+    function updateThumbnails() {
+
         const maxPerRow = getMaxPerRow()
         const newThumbnailsGrid = []
 
-        for (let i = 0; i < rows; i++){
+        for (let i = 0; i < rows; i++) {
             const rowThumbnails = []
-            for (let j = 0; j < maxPerRow; j++){
+            for (let j = 0; j < maxPerRow; j++) {
                 rowThumbnails.push(
                     <ArtworkThumbnail key={`${i}-${j}`} image={tempArtworkThumbnail} artist="goober" title="test" imageLink="/test/image" />
                 )
@@ -42,32 +36,30 @@ const ArtGrid = ({rows}) => {
 
         setThumbnails(newThumbnailsGrid)
     }
-
     useEffect(() => {
+
         updateThumbnails()
 
         const handleResize = () => {
             updateThumbnails();
         }
-        window.addEventListener("resize", handleResize); 
-        
+        window.addEventListener("resize", handleResize);
+
         return () => {
             window.removeEventListener("rezise", handleResize);
         }
-    
-    },[])
 
-    return(
+    }, [])
+
+    return (
         <div className="art-grid">
-
             {thumbnails.map((row, i) => (
                 <div key={`row${i}`} className="art-grid-row">
                     {row}
                 </div>
             ))}
-
         </div>
     )
 }
 
-export default ArtGrid
+export default ArtGrid;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,7 @@
 import "./Home.css"
 import SectionHeading from "../Components/SectionHeading/SectionHeading";
-import ThemeToggleButton from "../Components/ThemeToggleButton/ThemeToggleButton";
 import FeaturedArtistThumbnail from "../Components/FeaturedArtistThumbnail/FeaturedArtistThumbnail";
 import tempArtistImage from "../Assets/Temp/FeaturedArtist.png"
-import ArtworkThumbnail from "../Components/ArtworkThumbnail/ArtworkThumbnail";
-import tempArtworkThumbnail from "../Assets/Temp/ArtworkThumb.png"
 
 import ProductCard from "../Components/ProductCard/ProductCard";
 import ProductImage from "../Assets/Temp/ProductThumbnail.png";
