442efdc64cecd2bac11c930cada523d419758e59
Maciej-Clanet <maciejclanet@gmail.com>
2023-11-29 13:00:32 +0000

    struggle

--- src/Components/ArtGrid/ArtGrid.js
+++ src/Components/ArtGrid/ArtGrid.js
@@ -4,8 +4,10 @@ import tempArtworkThumbnail from "../../Assets/Temp/ArtworkThumb.png"
 
 import { useEffect } from "react"
 import { useState } from "react"
+import axios from "axios"
 
-const ArtGrid = ({ rows }) => {
+const ArtGrid = ({ rows, type = "new" }) => {
+    const [thumbnailsData, setThumbnailsData] = useState([])
     const [thumbnails, setThumbnails] = useState([]);
 
     function getMaxPerRow() {
@@ -19,29 +21,57 @@ const ArtGrid = ({ rows }) => {
         return maxItems
     }
 
-    function updateThumbnails() {
-
+    function updateThumbnails(data) {
+        console.log("received data: ", data)
         const maxPerRow = getMaxPerRow()
         const newThumbnailsGrid = []
+        let thumbIndex = 0
 
         for (let i = 0; i < rows; i++) {
             const rowThumbnails = []
             for (let j = 0; j < maxPerRow; j++) {
-                rowThumbnails.push(
-                    <ArtworkThumbnail key={`${i}-${j}`} image={tempArtworkThumbnail} artist="goober" title="test" imageLink="/test/image" />
-                )
+                const thumbnail = data[thumbIndex]
+                if(thumbnail){
+                    rowThumbnails.push(
+                        <ArtworkThumbnail key={`${i}-${j}`} image={tempArtworkThumbnail} artist="goober" title={thumbnail["title"]} imageLink="/test/image" />
+                    )
+                }else{
+                    rowThumbnails.push(<ArtworkThumbnail key={`${i}-${j}`} image={tempArtworkThumbnail} artist="goober" title="default" imageLink="/test/image" />)
+                }
+
+                thumbIndex++;
             }
             newThumbnailsGrid.push(rowThumbnails)
         }
-
         setThumbnails(newThumbnailsGrid)
     }
+
+
     useEffect(() => {
 
-        updateThumbnails()
+
+        function getData(){
+            if(type == "new"){
+                console.log("fetching new")
+                axios.post("http://localhost:8000/art/newest", { amount: rows * getMaxPerRow() } )
+                .then(res => {
+                    console.log(res.data)
+                    setThumbnailsData(res.data);
+                    updateThumbnails(res.data);
+                })
+                .catch(error => {
+                    console.error("error fetching thumbnails: ", error)
+                })
+            }
+        }
+        
+        getData()
+        // updateThumbnails()
 
         const handleResize = () => {
-            updateThumbnails();
+  
+                getData()
+            
         }
         window.addEventListener("resize", handleResize);
 
@@ -49,7 +79,7 @@ const ArtGrid = ({ rows }) => {
             window.removeEventListener("rezise", handleResize);
         }
 
-    }, [])
+    }, [thumbnailsData])
 
     return (
         <div className="art-grid">
@@ -63,3 +93,16 @@ const ArtGrid = ({ rows }) => {
 }
 
 export default ArtGrid;
+
+
+
+// function getThumbnailsData(){
+//     axios.post("http://localhost/art/newest", getMaxPerRow * rows)
+//     .then((res) => {
+//         alert(res)
+//         setThumbnailsData(res)
+//     })
+//     .catch((err) => {
+//         alert(err)
+//     })
+// }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,12 +9,14 @@ import AvatarImage from "../Assets/Temp/Avatar.png"
 
 import ArtGrid from "../Components/ArtGrid/ArtGrid";
 
+import axios from "axios";
+
 const Home = () => {
     return (
         <>
             <FeaturedArtistThumbnail artistName="goober" artistProfession="Unemployed" profileLink="/user/goober" image={tempArtistImage}/>
             <SectionHeading text="Trending this week"/>
-            <ArtGrid rows={2}/>
+            <ArtGrid rows={2} type="new"/>
             <SectionHeading text="Popular on the Marketplace" active={true}/>
             <ProductCard image={ProductImage} title="Test title" artist="goober" price={3.50} avatar={AvatarImage} productLink="/products/testproduct"/>
             
