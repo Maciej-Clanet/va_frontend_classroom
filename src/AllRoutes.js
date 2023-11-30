6c405310db3a73ed0ac710ba40ddf7179f05b317
Maciej-Clanet <maciejclanet@gmail.com>
2023-11-30 09:47:34 +0000

    updating some setup

--- src/AllRoutes.js
+++ src/AllRoutes.js
@@ -7,6 +7,9 @@ const AllRoutes = () => {
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/Learn" element={<Learn/>}/>
+            <Route path="/Login" element={<Login/>} />
+            <Route path="/Register" element={<Register/>} />
+            <Route path="/Profile" element={ <Profile/> } />
         </Routes>
     )
 }
